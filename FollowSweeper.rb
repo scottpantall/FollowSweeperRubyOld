42077a5c5ed5efd7cfd7f95fc3632ecb46aa3987
Scott Pantall <aspantall@gmail.com>
2015-02-03 06:33:20 -0700

    Keeps program from throwing too many exception error. creates file with list of friends to unfollow

--- FollowSweeper.rb
+++ FollowSweeper.rb
@@ -10,43 +10,58 @@ client = Twitter::REST::Client.new do |config|
   config.access_token_secret = MY_ACCESS_SECRET
 end
 
-# Awesome cool variables
-count = 0                 # Number of friends that haven't tweeted in 6 months
-oldfriends = []           # Array of old friends
 friends = client.friends  # All teh friends
+count = 0
+oldcount = 0
+oldfriends = []
+FILENAME = "oldfriends.txt"
 
-# Loop through friends, display old friends and add them to array
-begin
+if File.file?(FILENAME)
+  File.open(FILENAME, "r").each_line do |line| 
+    oldfriends << line 
+    oldPcount += 1
+  end
+else
+  outFile = File.new(FILENAME, "w")
+    
+  puts "Searching your friends list for friends who have not updated in over 6 months. This may take a while..."
+
+  # Loop through friends, display old friends and add them to array
   friends.each do |friend|
-    if friend.status.created_at < 6.months.ago
-      count += 1
-      puts "#{friend.screen_name}: #{friend.status.created_at}"
-      oldfriends << friend
+    count += 1
+    if(count < 300)
+      if friend.status.created_at < 6.months.ago
+        oldcount += 1
+        puts "#{friend.screen_name}: #{friend.status.created_at}"
+        outFile.puts "#{friend.screen_name}"
+        #oldfriends << friend.screen_name
+      end
+    else
+      time = Time.new
+      puts "Watchin... and waitin... #{time.inspect}"
+      sleep 901
+      count = 0
     end
-  end	
+  end 
 
-# If too many requests are made, wait until it can make more requests and retry
-# Should probably be made into a function
-rescue Twitter::Error::TooManyRequests => error
-  time = Time.new
-  puts "Watchin... and waitin... #{time.inspect}"
-  sleep error.rate_limit.reset_in + 1
-  retry
+  outFile.close()
 end
 
-# Since the retry gets dupicate twitter accounts, make sure there are no duplicates
-oldfriends.uniq!
+puts "I can unfollow #{oldcount} Twitter accounts"
 
-puts "I will unfollow #{count} Twitter accounts"
-puts "Listed accounts..."
+=begin
+puts "Unfollowing friends who haven’t updated in over 6 months…"
+count = 0
+
 
 # Loop through array of oldfriends. Unfollow those that are not in my "Friends" list
 begin
   oldfriends.each do |friend|
-    client.unfollow(friend) unless client.list_member? client.list(6956771), friend
+    count += 1
+    puts "#{count}"
+    client.unfollow(friend)
     puts "#{friend.screen_name} has been unfollowed"
   end
-
 # If too many requests are made, wait until it can make more requests and retry
 # Should probably be made into a function	
 rescue Twitter::Error::TooManyRequests => error
@@ -55,3 +70,4 @@ rescue Twitter::Error::TooManyRequests => error
   sleep error.rate_limit.reset_in + 1
   retry
 end
+=end
